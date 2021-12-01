c6e89de80dfbf60172bf8a16b190361ed0a9d029
BestGoofy <70072322+BestGoofy@users.noreply.github.com>
2021-12-01 18:14:04 +0100

    added day1

--- 2021/Python/Day 1/day1.py
+++ 2021/Python/Day 1/day1.py
@@ -1,4 +1,19 @@
 with open('input.txt', 'r') as f:
     text = f.readlines()
 input = [int(i) for i in text]
-print(input)
+result = 0
+input2 = []
+
+for y in range(len(input)):
+    if y < 3:
+        continue
+    else:
+        #print(input[y - 3], input[y - 2], input[y - 1])
+        input2.append(input[y - 3] + input[y - 2] + input[y - 1])
+
+for x in range(len(input2)):
+    if x == 0:
+        continue
+    elif input2[x] > input2[x - 1]:
+        result += 1
+print(result)

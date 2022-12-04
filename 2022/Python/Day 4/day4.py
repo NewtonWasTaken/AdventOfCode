7ceb9e89edf87f1fef5cf5e329f027d506fa3340
BestGoofy <70072322+BestGoofy@users.noreply.github.com>
2022-12-04 12:14:00 +0100

    edited Day 4

--- 2022/Python/Day 4/day4.py
+++ 2022/Python/Day 4/day4.py
@@ -1,6 +1,7 @@
 input = open("input.txt")
 text = [y.split("-") for x in [i.split(",") for i in input.read().splitlines()] for y in x]
 result = 0
+result2 = 0
 for x in range(len(text)):
     if (x + 1) % 2 == 0:
         for y in range(len(text[x])):
@@ -9,14 +10,7 @@ for x in range(len(text)):
                 second_set = set(list(range(int(text[x][y-1]), int(text[x][y]) + 1)))
                 if first_set.issubset(second_set) or second_set.issubset(first_set):
                     result += 1
-print(result)
-result2 = 0
-for x in range(len(text)):
-    if (x + 1) % 2 == 0:
-        for y in range(len(text[x])):
-            if y == 1:
-                first_set = set(list(range(int(text[x-1][y-1]), int(text[x-1][y]) + 1)))
-                second_set = set(list(range(int(text[x][y-1]), int(text[x][y]) + 1)))
                 if len(first_set.intersection(second_set)) != 0:
                     result2 += 1
+print(result)
 print(result2)

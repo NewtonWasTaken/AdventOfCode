27396ec62c0b8a58189537d2b151bff36fb8a958
BestGoofy <70072322+BestGoofy@users.noreply.github.com>
2022-12-04 11:48:28 +0100

    edited Day 4

--- 2022/Python/Day 4/day4.py
+++ 2022/Python/Day 4/day4.py
@@ -23,7 +23,6 @@ for x in range(len(text2)):
                 second_set = set(list(range(int(text2[x][y-1]), int(text2[x][y]) + 1)))
                 if len(first_set.intersection(second_set)) != 0 or len(second_set.intersection(first_set)) != 0:
                     result += 1
-    if (x + 1) % 2 == 0:
         first_set = set()
         second_set = set()
 print(result)

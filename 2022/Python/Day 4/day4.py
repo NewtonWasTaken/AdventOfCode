50c3d3aff79762376da3f326d84b5dd7e16038ed
BestGoofy <70072322+BestGoofy@users.noreply.github.com>
2022-12-04 11:57:19 +0100

    edited Day 4

--- 2022/Python/Day 4/day4.py
+++ 2022/Python/Day 4/day4.py
@@ -1,27 +1,26 @@
 input = open("input.txt")
-text = [i.split(",") for i in input.read().splitlines()]
-text2 = [y.split("-") for x in text for y in x]
+text = [y.split("-") for x in [i.split(",") for i in input.read().splitlines()] for y in x]
 result = 0
-for x in range(len(text2)):
+for x in range(len(text)):
     if (x + 1) % 2 == 0:
-        for y in range(len(text2[x])):
+        for y in range(len(text[x])):
             if y == 1:
-                first_set = set(list(range(int(text2[x-1][y-1]), int(text2[x-1][y]) + 1)))
-                second_set = set(list(range(int(text2[x][y-1]), int(text2[x][y]) + 1)))
+                first_set = set(list(range(int(text[x-1][y-1]), int(text[x-1][y]) + 1)))
+                second_set = set(list(range(int(text[x][y-1]), int(text[x][y]) + 1)))
                 if first_set.issubset(second_set) or second_set.issubset(first_set):
                     result += 1
         first_set = set()
         second_set = set()
 print(result)
-result = 0
-for x in range(len(text2)):
+result2 = 0
+for x in range(len(text)):
     if (x + 1) % 2 == 0:
-        for y in range(len(text2[x])):
+        for y in range(len(text[x])):
             if y == 1:
-                first_set = set(list(range(int(text2[x-1][y-1]), int(text2[x-1][y]) + 1)))
-                second_set = set(list(range(int(text2[x][y-1]), int(text2[x][y]) + 1)))
+                first_set = set(list(range(int(text[x-1][y-1]), int(text[x-1][y]) + 1)))
+                second_set = set(list(range(int(text[x][y-1]), int(text[x][y]) + 1)))
                 if len(first_set.intersection(second_set)) != 0 or len(second_set.intersection(first_set)) != 0:
-                    result += 1
+                    result2 += 1
         first_set = set()
         second_set = set()
-print(result)
+print(result2)
